dc2d81502c2e55a7ef38b44b4add7e6d62543364
nikolajgorovikov <gorovikovkg@gmail.com>
2021-06-23 03:19:41 +0300

    commit

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,12 +5,12 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const params = {
+    asc: 1,
+    desc: -1
+  };
   const collator = new Intl.Collator(undefined, {caseFirst: "upper"});
-  let answer = arr.sort(collator.compare);
-  // (item1, item2)=>{
-  //     return item1.localeCompare(item2, {kf: "lower"});
-  //   }
-  return param === "asc" ? answer : answer.reverse();
+  return Array.from(arr.sort((i,j)=>params[param] * collator.compare(i,j)));
 }
 
 
