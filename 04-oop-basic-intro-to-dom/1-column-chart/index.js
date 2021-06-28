9829aca034c346ddf3f0c2559194beba7913958a
nikolajgorovikov <gorovikovkg@gmail.com>
2021-06-29 02:59:36 +0300

    commit

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,14 +1,29 @@
 // А будем ли мы реализовывать такие вещи с помощью кастомных элементов и вообще проходить их?
 export default class ColumnChart {
     constructor(obj = {}) {
-        this.element = document.createElement("div");
-        this.element.classList.add("column-chart");
-        this.element.style.cssText = "--chart-height: 50";
-        this.update(obj);
+        this.create(obj);
+    }
+    create(obj){
+      this.element = document.createElement("div");
+      this.element.classList.add("column-chart");
+      this.element.style.cssText = "--chart-height: 50";
+      this.update(obj);
     }
     chartHeight = 50;
     update({data = [], label = "untitled", value = 0, link, formatHeading = (data) => data}){
-      const colons = makeColons(data);
+      const colons = this.createColons(data);
+      this.createInner(colons, label, value, link, formatHeading);
+      this.createData(data);
+    }
+    createColons(data){
+      if (!data.length) return "";
+      let result = "";
+      for (let {percent, value} of getColumnProps(data)){
+        result+=`<div style="--value: ${value}" data-tooltip="${percent}"></div>`
+      }
+      return result;
+    }
+    createInner(colons, label, value, link, formatHeading){
       this.element.innerHTML = `
       <div class="column-chart__title">
         Total ${label}
@@ -23,12 +38,14 @@ export default class ColumnChart {
          </div>
       </div>
         `;
+    }
+    createData(data){
       if (!data.length) this.element.classList.add("column-chart_loading");
       else this.element.className = "column-chart";
     }
     destroy() {
       this.element.remove();
-      delete this.element;
+      this.element = null;
     }
     remove() {
       this.element.remove();
@@ -44,12 +61,4 @@ function getColumnProps(data) {
       value: String(Math.floor(item * scale))
     };
   });
-}
-function makeColons(data){
-  if (!data.length) return "";
-  let result = "";
-  for (let {percent, value} of getColumnProps(data)){
-    result+=`<div style="--value: ${value}" data-tooltip="${percent}"></div>`
-  }
-  return result;
 }
