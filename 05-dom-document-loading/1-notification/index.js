cf796c85ea5576b9520d85c9438384ec97937378
nikolajgorovikov <gorovikovkg@gmail.com>
2021-07-03 19:01:37 +0300

    commit

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,39 @@
 export default class NotificationMessage {
-
+  static actualMessage
+  static removeMessage() {
+    this.actualMessage.remove();
+    clearTimeout(this.actualMessage.removeTimeout);
+    this.actualMessage = null;
+  }
+  constructor(message = "", {duration = 2000, type = "success"} = {}) {
+    this.element = document.createElement("div");
+    this.createInner(message, {duration, type});
+  }
+  createInner(message = '', {duration = 2000, type = "success"} = {}) {
+    this.element.innerHTML = `
+    <div class="notification ${type}" style="--value:${duration / 1000}s">
+      <div class="timer"></div>
+      <div class="inner-wrapper">
+        <div class="notification-header">${type}</div>
+        <div class="notification-body">${message}</div>
+      </div>
+    </div>`;
+    this.element = this.element.firstElementChild;
+    this.duration = duration;
+  }
+  show(target = document.body) {
+    if (NotificationMessage.actualMessage) {
+      NotificationMessage.removeMessage();
+    }
+    NotificationMessage.actualMessage = this.element;
+    this.element.removeTimeout = setTimeout(()=>{
+      this.element.remove();
+    }, this.duration);
+    target.append(this.element);
+  }
+  destroy() {
+    this?.element?.remove();
+    clearTimeout(this?.element?.removeTimeout);
+    this.element = null;
+  }
 }
