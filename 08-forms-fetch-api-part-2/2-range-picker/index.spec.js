4872760876012c1fcfc268873c4914708d5e51c8
nikolajgorovikov <gorovikovkg@gmail.com>
2021-07-19 02:55:44 +0300

    product form v2 and sortable list

--- 08-forms-fetch-api-part-2/2-range-picker/index.spec.js
+++ 08-forms-fetch-api-part-2/2-range-picker/index.spec.js
@@ -138,11 +138,8 @@ describe('forms-fetch-api-part-2/range-picker', () => {
     let from = rangePicker.element.querySelector('.rangepicker__selected-from');
     const prevDate = from.previousElementSibling;
     const nextDate = from.nextElementSibling;
-    console.log(rangePicker.actualDateRange.from, rangePicker.actualDateRange.to);
     prevDate.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    console.log(rangePicker.firstSpot);
     nextDate.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-    console.log(rangePicker.actualDateRange.from, rangePicker.actualDateRange.to);
     from = rangePicker.element.querySelector('.rangepicker__selected-from');
     const to = rangePicker.element.querySelector('.rangepicker__selected-to');
 
